8296c79ab402cb758e0a209b7ddf20c4fc6c91be
Alexander Larin <scalar438@gmail.com>
2019-10-06 22:01:42 +0300

    Remove test code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[macro_use]
 extern crate futures;
 extern crate telegram_bot;
 extern crate tokio_core;
@@ -7,54 +6,8 @@ extern crate tokio;
 
 use futures::Stream;
 use telegram_bot::*;
-use tokio::timer::Interval;
 use tokio_core::reactor::Core;
 
-use std::time::Duration;
-
-#[derive(Debug)]
-pub struct Fibonacci {
-	interval: Interval,
-	curr: u64,
-	next: u64,
-}
-
-impl Fibonacci {
-	fn new(duration: Duration) -> Fibonacci {
-		Fibonacci {
-			interval: Interval::new_interval(duration),
-			curr: 1,
-			next: 1,
-		}
-	}
-}
-
-impl Stream for Fibonacci {
-	type Item = u64;
-
-	// The stream will never yield an error
-	type Error = ();
-
-	fn poll(&mut self) -> futures::Poll<Option<u64>, ()> {
-		// Wait until the next interval
-		try_ready!(self
-			.interval
-			.poll()
-			// The interval can fail if the Tokio runtime is unavailable.
-			// In this example, the error is ignored.
-			.map_err(|_| ()));
-
-		let curr = self.curr;
-		let next = curr + self.next;
-
-		self.curr = self.next;
-		self.next = next;
-
-		Ok(Async::Ready(Some(curr)))
-	}
-}
-
-use futures::Async;
 use futures::Future;
 
 fn main() {
@@ -85,11 +38,5 @@ fn main() {
 		})
 		.map_err(|_| ());
 
-	let fib_stream = Fibonacci::new(Duration::from_secs(3)).for_each(|x| {
-		println!("{:?}", x);
-
-		Ok(())
-	});
-
-	core.run(fib_stream.join(future)).unwrap();
+	core.run(future).unwrap();
 }
