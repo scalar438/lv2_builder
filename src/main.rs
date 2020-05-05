f40e36b2c29e866c024c0d5f74e4db2e8be41716
Alexander Larin <scalar438@gmail.com>
2020-05-05 22:05:19 +0300

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate telegram_bot;
 extern crate tokio_core;
 
 use futures::{Future, Stream};
+use std::collections::HashMap;
 use telegram_bot::types::refs::UserId;
 use telegram_bot::*;
 use tokio_core::reactor::Core;
@@ -67,14 +68,100 @@ fn try_get_creator_id() -> Option<UserId> {
 		.map(UserId::new)
 }
 
+type UserActions = HashMap<sysinfo::Pid, activity::ActivityKind>;
+type AllActions = HashMap<UserId, UserActions>;
+
+struct BotData {
+	creator_id: Option<UserId>,
+	logger: logger::Logger,
+
+	api: telegram_bot::Api,
+	subscribers: AllActions,
+}
+
+impl BotData {
+	fn process_message(&mut self, msg: &str, chat: &telegram_bot::types::User) {
+		let logger_msg = if Some(chat.id) == self.creator_id {
+			"Creator message".to_string()
+		} else {
+			format!("User message, name: {}, id: {}", chat.first_name, chat.id)
+		};
+
+		let logger_msg = format!("<{}>: {}", logger_msg, msg);
+		let request_type = Request::new(msg);
+		let s = match request_type {
+			Request::Help => SendMessage::new(chat, get_string_help()),
+
+			Request::Check | Request::Subscribe => {
+				let act_list = activity::get_activity_list();
+				let s = if let Some(elem) = act_list.first() {
+					// Has at least one element
+
+					let mut msg = if act_list.len() == 1 {
+						format!("Current action: {}", elem.activity)
+					} else {
+						"There are many actions".to_owned()
+					};
+					if request_type == Request::Subscribe {
+						msg += "\n";
+						msg += "When the action completed you will be notified";
+
+						let h: std::collections::HashMap<_, _> =
+							act_list.into_iter().map(|a| (a.pid, a.activity)).collect();
+
+						self.subscribers.insert(chat.id, h);
+					}
+
+					msg
+				} else {
+					"There is no current actions".to_owned()
+				};
+
+				SendMessage::new(chat, s)
+			}
+
+			Request::UnknownRequest(_) => SendMessage::new(
+				chat,
+				format!("Unknown command: {}. \n{}", msg, get_string_help()),
+			),
+		};
+		self.logger.write(&logger_msg);
+
+		self.api.spawn(s);
+	}
+
+	fn process_timer(&mut self) {
+		let current_actions = activity::get_activity_list();
+		let pid_list_new = current_actions.iter().map(|a| &a.pid).collect();
+
+		for (chat, actions) in self.subscribers.iter_mut() {
+			let pid_list_old: std::collections::HashSet<_> = actions.keys().collect();
+			assert_ne!(pid_list_old.len(), 0);
+
+			let completed_list: Vec<_> = pid_list_old
+				.difference(&pid_list_new)
+				.map(|a| **a)
+				.collect();
+			for pid in completed_list {
+				if let Some(act) = actions.get(&pid) {
+					self.api
+						.spawn(SendMessage::new(chat.clone(), format!("{} completed", act)));
+				}
+				actions.remove(&pid);
+			}
+		}
+		self.subscribers.retain(|_, actions| actions.len() != 0);
+	}
+}
+
 fn main() {
 	let token = std::env::var("TELEGRAM_BOT_TOKEN").unwrap();
 	let creator_id = try_get_creator_id();
 
 	let mut core = Core::new().unwrap();
-	let subscribers = std::cell::RefCell::new(std::collections::HashMap::new());
-	let mut logger = logger::Logger::new(std::env::args().find(|a| a == "-no_log_file").is_none());
-	let api = Api::configure(token.clone()).build(core.handle()).unwrap();
+	let subscribers = HashMap::new();
+	let logger = logger::Logger::new(std::env::args().find(|a| a == "-no_log_file").is_none());
+	let api = Api::configure(token).build(core.handle()).unwrap();
 
 	if let Some(creator_id) = creator_id {
 		let user = telegram_bot::chat::User {
@@ -89,67 +176,29 @@ fn main() {
 		api.spawn(SendMessage::new(chat, "Bot started"));
 	}
 
+	let bot_data = BotData {
+		api,
+		subscribers,
+		creator_id,
+		logger,
+	};
+	let bot_data = std::cell::RefCell::new(bot_data);
+
 	loop {
-		logger.write("-----------------------------\nBot started");
+		bot_data
+			.borrow_mut()
+			.logger
+			.write("-----------------------------\nBot started");
 		// Fetch new updates via long poll method
-		let message_process = api
+		let message_process = bot_data
+			.borrow()
+			.api
 			.stream()
 			.for_each(|update| {
 				// If the received update contains a new message...
 				if let UpdateKind::Message(message) = update.kind {
 					if let MessageKind::Text { ref data, .. } = message.kind {
-						let logger_msg = if Some(message.from.id) == creator_id {
-							"Creator message".to_string()
-						} else {
-							format!(
-								"User message, name: {}, id: {}",
-								message.from.first_name, message.from.id
-							)
-						};
-
-						let logger_msg = format!("<{}>: {}", logger_msg, data);
-						let request_type = Request::new(data);
-						let s = match request_type {
-							Request::Help => SendMessage::new(message.chat, get_string_help()),
-
-							Request::Check | Request::Subscribe => {
-								let act_list = activity::get_activity_list();
-								let s = if let Some(elem) = act_list.first() {
-									// Has at least one element
-
-									let mut msg = if act_list.len() == 1 {
-										format!("Current action: {}", elem.activity)
-									} else {
-										"There are many actions".to_owned()
-									};
-									if request_type == Request::Subscribe {
-										msg += "\n";
-										msg += "When the action completed you will be notified";
-
-										let h: std::collections::HashMap<_, _> = act_list
-											.into_iter()
-											.map(|a| (a.pid, a.activity))
-											.collect();
-
-										subscribers.borrow_mut().insert(message.chat.clone(), h);
-									}
-
-									msg
-								} else {
-									"There is no current actions".to_owned()
-								};
-
-								SendMessage::new(message.chat, s)
-							}
-
-							Request::UnknownRequest(_) => SendMessage::new(
-								message.chat,
-								format!("Unknown command: {}. \n{}", data, get_string_help()),
-							),
-						};
-						logger.write(&logger_msg);
-
-						api.spawn(s);
+						bot_data.borrow_mut().process_message(&data, &message.from);
 					}
 				}
 
@@ -159,27 +208,7 @@ fn main() {
 
 		let status_timer = tokio::timer::Interval::new_interval(std::time::Duration::from_secs(10))
 			.map(|_| {
-				let current_actions = activity::get_activity_list();
-				let pid_list_new = current_actions.iter().map(|a| &a.pid).collect();
-
-				let mut subscribers = subscribers.borrow_mut();
-
-				for (chat, actions) in subscribers.iter_mut() {
-					let pid_list_old: std::collections::HashSet<_> = actions.keys().collect();
-					assert_ne!(pid_list_old.len(), 0);
-
-					let completed_list: Vec<_> = pid_list_old
-						.difference(&pid_list_new)
-						.map(|a| **a)
-						.collect();
-					for pid in completed_list {
-						if let Some(act) = actions.get(&pid) {
-							api.spawn(SendMessage::new(chat.clone(), format!("{} completed", act)));
-						}
-						actions.remove(&pid);
-					}
-				}
-				subscribers.retain(|_, actions| actions.len() != 0);
+				bot_data.borrow_mut().process_timer();
 			})
 			.for_each(|_| Ok(()))
 			.map_err(|_| ());
