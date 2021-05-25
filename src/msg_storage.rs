f949a44d482fe35ba8a0b8e3fa5a44f95844dfc3
Alexander Larin <scalar438@gmail.com>
2021-05-25 17:42:56 +0300

    Load/save messages (do not tested)

--- src/msg_storage.rs
+++ src/msg_storage.rs
@@ -1,11 +1,41 @@
-pub struct MessageStorage<T: Eq + Ord> {
+use std::io::{Read, Write};
+use std::str::FromStr;
+
+pub struct MessageStorage<T: Eq + Ord + serde::Serialize + for<'a> serde::Deserialize<'a>> {
 	msg_list: Vec<(T, chrono::DateTime<chrono::Utc>)>,
 }
 
-impl<T: Eq + Ord + Clone> MessageStorage<T> {
+impl<T: Clone + Eq + Ord + serde::Serialize + for<'a> serde::Deserialize<'a>> MessageStorage<T> {
 	pub fn new() -> Self {
-		Self {
-			msg_list: Vec::new(),
+		match std::fs::File::open(MessageStorage::<T>::get_file_path()) {
+			Ok(mut f) => return MessageStorage::new_from_file(&mut f),
+			Err(_) => {
+				return Self {
+					msg_list: Vec::new(),
+				}
+			}
+		}
+	}
+
+	fn new_from_file(f: &mut std::fs::File) -> Self {
+		let mut data = Vec::new();
+		f.read_to_end(&mut data).unwrap();
+		if let Ok(msg_list) = serde_json::from_slice::<Vec<(T, String)>>(&data) {
+			Self {
+				msg_list: msg_list
+					.into_iter()
+					.filter_map(|(id, date_str)| {
+						match chrono::DateTime::<chrono::Utc>::from_str(&date_str) {
+							Ok(date) => Some((id, date)),
+							Err(_) => None,
+						}
+					})
+					.collect(),
+			}
+		} else {
+			Self {
+				msg_list: Vec::new(),
+			}
 		}
 	}
 
@@ -34,6 +64,7 @@ impl<T: Eq + Ord + Clone> MessageStorage<T> {
 			}
 		}
 		self.msg_list.push((msg_id, chrono::Utc::now()));
+		self.write_messages().ok();
 	}
 
 	pub fn remove_messages(&mut self, msg_list: Vec<T>) {
@@ -42,23 +73,62 @@ impl<T: Eq + Ord + Clone> MessageStorage<T> {
 
 		self.msg_list
 			.retain(|(id, _)| msg_list.binary_search(id).is_err());
+
+		self.write_messages().ok();
+	}
+
+	fn write_messages(&self) -> Result<(), std::io::Error> {
+		let mut path = std::env::current_exe().unwrap();
+		path.pop();
+		path.push("message.json");
+		let mut f = std::fs::OpenOptions::new()
+			.write(true)
+			.create(true)
+			.open(MessageStorage::<T>::get_file_path())?;
+
+		self.write_messages_to_file(&mut f)
+	}
+
+	fn get_file_path() -> std::path::PathBuf {
+		let mut path = std::env::current_exe().unwrap();
+		path.pop();
+		path.push("message.json");
+
+		path
+	}
+
+	fn write_messages_to_file(&self, f: &mut std::fs::File) -> Result<(), std::io::Error> {
+		let v: Vec<_> = self
+			.msg_list
+			.iter()
+			.map(|(id, date)| (id, date.to_string()))
+			.collect();
+
+		let json = serde_json::to_string(&v).unwrap();
+		f.write(json.as_bytes()).map(|_| {})
 	}
 }
 
-#[test]
-fn test_1() {
-	let mut msg = MessageStorage::new();
-	msg.add_message(1);
-	msg.add_message(2);
-	std::thread::sleep(std::time::Duration::from_secs(1));
-	msg.add_message(3);
-	assert_eq!(
-		msg.get_old_messages(&chrono::Duration::milliseconds(500)),
-		[1, 2]
-	);
-	msg.remove_messages(vec![1]);
-	assert_eq!(
-		msg.get_old_messages(&chrono::Duration::milliseconds(500)),
-		[2]
-	);
+#[cfg(test)]
+mod test {
+
+	use super::*;
+
+	#[test]
+	fn test_1() {
+		let mut msg = MessageStorage::new();
+		msg.add_message(1);
+		msg.add_message(2);
+		std::thread::sleep(std::time::Duration::from_secs(1));
+		msg.add_message(3);
+		assert_eq!(
+			msg.get_old_messages(&chrono::Duration::milliseconds(500)),
+			[1, 2]
+		);
+		msg.remove_messages(vec![1]);
+		assert_eq!(
+			msg.get_old_messages(&chrono::Duration::milliseconds(500)),
+			[2]
+		);
+	}
 }
